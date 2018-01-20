d8efbfb857a418c2f0177b9693eef8b0e3ade20d
OlegRyz <aleh.ryzhankou@gmail.com>
2018-01-20 18:38:48 +0100

    Initial commit for OlegSPY lib

---
+++ src/main/java/org/niksi/olegspy/Tag.java
@@ -0,0 +1,11 @@
+package org.niksi.olegspy;
+
+/**
+ * This is class that contains OlegSPY tag
+ */
+public class Tag {
+    /**
+     * Tag for logger that shows that this is debug log
+     */
+    public static final String OlegSPY = "OlegSPY";
+}
